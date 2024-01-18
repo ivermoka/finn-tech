d751ba93995b4c0af74f4a4332fd9eae8be8a42d
ivermoka <ivermoka@icloud.com>
2024-01-18 13:11:53 +0100

    changed go file name

---
+++ api/index.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	// "github.com/gorilla/mux"
+)
+
+func getItemsHandler(w http.ResponseWriter, r *http.Request) {
+	items := GetItems()
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
+}
+
+func main() {
+	Connect()
+	http.HandleFunc("/api/items", getItemsHandler)
+
+	port := "8080"
+	println("Server is running on port " + port)
+	http.ListenAndServe(":"+port, nil)
+}

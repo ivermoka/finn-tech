7e9c4252913f4c01d8646df9ef8e9d4bc8eadc34
ivermoka <ivermoka@icloud.com>
2024-01-23 14:24:46 +0100

    content type added backend

--- website/api/index.go
+++ website/api/index.go
@@ -62,6 +62,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	server := New()
+	w.Header().Set("Content-Type", "application/json")
 
 	server.GET("/items", func(context *Context) {
 		items := getItems()

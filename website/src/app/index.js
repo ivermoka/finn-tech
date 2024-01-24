dd3687da01301ce69046a11b2606c13f99104dff
ivermoka <ivermoka@icloud.com>
2024-01-24 11:52:56 +0100

    testtjt12j

--- website/src/app/index.js
+++ website/src/app/index.js
@@ -17,7 +17,7 @@ export async function fetchItems() {
 
     const data = await response.json();
     console.log("Fetched data:", data);
-    return "data";
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
     return null;

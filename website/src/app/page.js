c8fe8f05b0c69ac6611fd00e9a2e5de26176bfe2
ivermoka <ivermoka@icloud.com>
2024-01-24 12:08:02 +0100

    trying something

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,8 +1,31 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import BarChart from "./BarChart";
-import { fetchItems } from ".";
 import "./globals.css";
+
+async function fetchItems() {
+  try {
+    const response = await fetch("/api", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Server Response:", response);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("Fetched data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
 
 function App() {
   const [items, setItems] = useState([]);

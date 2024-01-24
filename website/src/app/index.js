5ab8a4cf5797972dcda92deab004dda9569b2895
ivermoka <ivermoka@icloud.com>
2024-01-24 10:02:22 +0100

    logging..:

--- website/src/app/index.js
+++ website/src/app/index.js
@@ -11,6 +11,7 @@ async function fetchItems() {
     .then((response) => response.json().then(console.log(response)))
     .then((data) => items.push(data))
     .catch((error) => console.log("Error fetching data: ", error, items));
+  console.log(items);
   return items;
 }
 export default fetchItems;
